f051822664c2c7a1f602d16f7f8b9370ed5267f0
Michael Sanders <michael.sanders@fastmail.com>
2019-12-22 21:25:12 -0800

    [shell] Add command for removing .DS_Store files

--- symlinks/fish/.config/fish/config.fish
+++ symlinks/fish/.config/fish/config.fish
@@ -74,4 +74,35 @@ function md --description "Make directory and switch to it"
     mkdir -p $argv && cd $argv
 end
 
+# From https://stackoverflow.com/a/39891882
+function read_confirm --description "Ask user for a confirmation" --argument prompt
+    if test -z "$prompt"
+        set prompt "Continue?"
+    end
+
+    while true
+        read -P "$prompt [y/N]: " -l confirm
+
+        switch $confirm
+            case Y y
+                return 0
+            case '' N n
+                return 1
+        end
+    end
+end
+
+
+function rm_ds_store --description "Remove .DS_Store files recursively from a directory" --argument dir
+    set --local files (fd --hidden '^\.DS_Store$' --no-ignore-vcs $dir)
+    if test -n "$files"
+        echo "Found $files"
+        if read_confirm "Remove files?"
+            rm $files
+        end
+    else
+        echo "No .DS_Store files found."
+    end
+end
+
 fish_hybrid_key_bindings
