0393cb92f8ba1f2139fc4bf66eb0c8e1e963dc5c
Michael Sanders <michael.sanders@fastmail.com>
2020-01-02 12:11:19 -0800

    Add license

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ For a full list of steps done on new machines, see
 
 This repo uses [zero.sh](https://github.com/zero-sh/zero.sh) for system setup
 and configuration.
+
+Licensed under the MIT License, except where otherwise noted (e.g., Emacs files
+are licensed under GPLv3). See [LICENSE](LICENSE) for more information.
--- symlinks/emacs/.spacemacs.d/layers/michaelsanders-better-defaults/keybindings.el
+++ symlinks/emacs/.spacemacs.d/layers/michaelsanders-better-defaults/keybindings.el
@@ -1,8 +1,25 @@
 ;;; keybindings.el --- Spacemacs Layer Key Bindings File
 ;;
+;; Copyright 2018 Michael Sanders.
+;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; License: GPLv3
+;; License:
+;;
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <https://www.gnu.org/licenses/>.
+;;
 
 ;; Evil bindings.
 (modify-syntax-entry ?_ "w") ; Treat underscores as part of word.
