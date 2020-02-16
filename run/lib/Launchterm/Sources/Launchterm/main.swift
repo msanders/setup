84cc59f18ddbfe5d52091856cf83dd461f3967f1
Michael Sanders <michael.sanders@fastmail.com>
2020-02-15 21:48:47 -0800

    [terminal] Disable toggling behavior in Launchterm

--- run/lib/Launchterm/Sources/Launchterm/main.swift
+++ run/lib/Launchterm/Sources/Launchterm/main.swift
@@ -4,12 +4,7 @@ import HotKey
 func main() {
     let hotKey = HotKey(key: .space, modifiers: [.option])
     hotKey.keyDownHandler = {
-        let workspace = NSWorkspace.shared
-        if workspace.frontmostApplication?.bundleIdentifier == "io.alacritty" {
-            workspace.launchApplication("Emacs")
-        } else {
-            workspace.launchApplication("Alacritty")
-        }
+        NSWorkspace.shared.launchApplication("Alacritty")
     }
 
     NSApplication.shared.run()
