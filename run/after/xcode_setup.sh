36cace003930226696ed81cad551541cc61f6355
Michael Sanders <michael.sanders@fastmail.com>
2020-08-31 15:41:22 -0700

    [swift] Update Swift & Xcode scripts for Catalina

--- run/after/xcode_setup.sh
+++ run/after/xcode_setup.sh
@@ -2,7 +2,9 @@
 set -o errexit -o nounset
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+TARGET_DIR="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/"
 
 echo "Installing Xcode theme..."
-cp "$SCRIPT_DIR/../lib/assets/"*.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+mkdir -p "$TARGET_DIR"
+cp "$SCRIPT_DIR/../lib/assets/"*.xccolortheme "$TARGET_DIR"
 echo "Successfully installed Xcode theme."
