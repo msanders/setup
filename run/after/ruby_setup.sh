8ec38650a9f095da4d8d1df165397457cea51019
Michael Sanders <michael.sanders@fastmail.com>
2020-08-31 15:33:03 -0700

    [ruby] Update Ruby version

--- run/after/ruby_setup.sh
+++ run/after/ruby_setup.sh
@@ -2,6 +2,7 @@
 set -o errexit -o nounset
 
 export RBENV_ROOT=/usr/local/var/rbenv
+
 if command -v rbenv >/dev/null; then
     eval "$(rbenv init -)"
 else
@@ -10,7 +11,7 @@ else
 fi
 
 echo "Updating Ruby"
-rbenv install 2.6.5 --skip-existing
-rbenv global 2.6.5
+rbenv install 2.6.6 --skip-existing
+rbenv global 2.6.6
 gem update --system
 gem install pry rubocop
