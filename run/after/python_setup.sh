29b4add58c1bc70d0c7ca455d9a45c17119acbab
Michael Sanders <michael.sanders@fastmail.com>
2020-10-15 16:50:13 -0700

    [python] Update Python versions

--- run/after/python_setup.sh
+++ run/after/python_setup.sh
@@ -3,9 +3,10 @@ eval "$(pyenv init -)"
 set -o errexit
 
 pyenv install 2.7.18 --skip-existing
-pyenv install 3.6.11 --skip-existing
-pyenv install 3.7.8 --skip-existing
-pyenv install 3.8.5 --skip-existing
+pyenv install 3.6.12 --skip-existing
+pyenv install 3.7.9 --skip-existing
+pyenv install 3.8.6 --skip-existing
+pyenv install 3.9.0 --skip-existing
 
 install_common_dependencies() {
     pip install -U pip setuptools virtualenv pipenv pytest nose pyflakes isort \
@@ -18,13 +19,16 @@ install_py3_dependencies() {
 
 pyenv shell 2.7.18
 install_common_dependencies
-pyenv shell 3.6.11
+pyenv shell 3.6.12
 install_common_dependencies
 install_py3_dependencies
-pyenv shell 3.7.8
+pyenv shell 3.7.9
 install_common_dependencies
 install_py3_dependencies
-pyenv shell 3.8.5
+pyenv shell 3.8.6
 install_common_dependencies
 install_py3_dependencies
-pyenv global 3.8.5
+pyenv shell 3.9.0
+install_common_dependencies
+install_py3_dependencies
+pyenv global 3.9.0
